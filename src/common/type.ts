7fd91a7027338c0d542d4053ee09056a374775b6
linqirong <linqirong@kuaishou.com>
2024-05-26 00:58:12 +0800

    feat: ui

--- src/common/type.ts
+++ src/common/type.ts
@@ -1,4 +1,5 @@
 export type IMainWindowFiles = {
+  iconPath: any;
   appIcon: string;
   filePath: string;
   fileName: string;
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { IMainWindowFiles } from "src/common/type";
+import { ArrowRight } from "./icon";
 
 function App(): JSX.Element {
-  const [files, setFiles] = useState<IMainWindowFiles>([]);
+  const [files, ] = useState<IMainWindowFiles>([]);
   const [active, setActive] = useState(0);
   const [input, setInput] = useState("");
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,13 +23,45 @@ function App(): JSX.Element {
   };
   return (
     <div>
-      <div>
-        <input type="text" onKeyDown={handleKeyDown} onChange={handleInput} value={input}></input>123
+      <div className={`relative flex pl-[20px] w-[100vw] items-center h-[50px] border-b-[1px] border-slate-300`}>
+        <input
+          type="text"
+          onKeyDown={handleKeyDown}
+          onChange={handleInput}
+          value={input}
+          className="mx-4 pl-[4px] h-full text-xl outline-none flex-1"
+        ></input>
+        123
       </div>
       <div>
-        <ul>
-          {files.map((file) => {
-            return <li>{file.fileName}</li>;
+        <ul className="px-[10px] my-[6px] overflow-y-scroll h-[300px]">
+          {files.map((file, i) => {
+            return (
+              <li
+              className={`flex [&>svg]:mx-[6px] [&>svg]:w-[18px] [&>svg]:h-[18px] items-center  pl-1 h-[34px] ${
+                i === active ? 'active-li' : ''
+              }`}
+              key={file.filePath + i}
+            >
+              {file?.appIcon && (
+                <>
+                  <img
+                    style={{
+                      maxHeight: '30px'
+                    }}
+                    src={'data:image/png;base64,' + file.appIcon}
+                  />
+                  <ArrowRight />
+                </>
+              )}
+              <span className="mx-[6px]">
+                {file.iconPath && (
+                  <img className="h-[20px]" src={'data:image/png;base64,' + file.iconPath} />
+                )}
+              </span>
+              <span className="text-lg">{file.fileName.replace('.app', '')} </span>
+            </li>
+            );
           })}
         </ul>
       </div>
