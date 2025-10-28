3af41b5a1956fdfc067fe15ea487f9e82e8f566a
qirong77 <qirong7777@gmail.com>
2025-10-28 18:28:23 +0800

    feat: 功能开发 ✨

--- src/preload/PreloadComponent/autoLogin.ts
+++ src/preload/PreloadComponent/autoLogin.ts
@@ -1,28 +1,4 @@
 import { showToast } from "../utils/toast";
 
 export function autoLogin() {
-  const url1 = "m@e.xi@aoj@uke@j@i.com".replaceAll("@", "");
-  const url2 = "m@e-a@u@t@@@@@h.i@n@@@t@r@@@@@a.x@@@i@@@@@a@@o@j@u@k@e@j@i.c@o@m@".replaceAll(
-    "@",
-    ""
-  );
-
-  if (window.origin.includes(url1) || window.origin.includes(url2)) {
-    showToast({ message: "检测到登录页面，正在自动填写登录信息...", type: "info" });
-    const user = document.querySelector("#username") as HTMLInputElement;
-    const password = document.querySelector("#password") as HTMLInputElement;
-    user.value = import.meta.env.PRELOAD_VITE_USER_NAME;
-    password.value = import.meta.env.PRELOAD_VITE_PASS_WORD;
-    const loginBtn = document.querySelector("#submit") as HTMLButtonElement;
-    loginBtn.click();
-  }
 }
-
-function autoLoginSassPre() {
-  if(window.location.href.includes('window.location.href')) {
-    
-  }
-}
-function autoLoginSassProd() {
-
-} 
